fe76928bca07d2638895169e3398cbbad2ebda0f
Nils Hagemann <mail@nilshagemann.de>
2017-04-29 18:07:21 +0200

    data dimension test for content controller

--- tests/AnyContent/Service/V1Controller/ContentControllerTest.php
+++ tests/AnyContent/Service/V1Controller/ContentControllerTest.php
@@ -42,13 +42,62 @@ class ContentControllerTest extends AbstractTest
                 $repository->saveRecord($record);
             }
 
-            $json = $this->getJsonResponse('/1/test/content/content1/records');
+            $json = $this->getJsonResponse('/1/test/content/content1/records', 200,
+                ['language' => $test->getLanguage()]);
 
             $this->assertArrayHasKey('records', $json);
             $this->assertCount(5, $json['records']);
 
+            $this->assertArrayHasKey('info', $json);
+            $info = $json['info'];
+
+            $this->assertEquals($test->getWorkspace(), $info['workspace']);
+            $this->assertEquals($test->getLanguage(), $info['language']);
+            $this->assertEquals(5, $info['count']);
+            $this->assertEquals('content1', $info['content_type']);
         }
 
     }
 
+
+    public function testGetRecordsDifferentViews()
+    {
+        echo 'TODO';
+    }
+
+
+    public function testGetOrderedRecords()
+    {
+        echo 'TODO';
+    }
+
+
+    public function testGetFilteredRecords()
+    {
+        echo 'TODO';
+    }
+
+
+    public function testGetSortedRecords()
+    {
+        echo 'TODO';
+    }
+
+
+    public function testAddRecords()
+    {
+        echo 'TODO';
+    }
+
+
+    public function testDeleteRecord()
+    {
+        echo 'TODO';
+    }
+
+
+    public function testDeleteRecords()
+    {
+        echo 'TODO';
+    }
 }
